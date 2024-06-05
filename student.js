d7e9d322250e39b4e0108e01a68cafbe00febfba
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-06-05 15:03:09 +0600

    05/06/24

---
+++ student.js
@@ -0,0 +1,9 @@
+const student_info = () => {
+    let name = "Arif";
+    let address = "Basabo";
+    return `name: ${name} <br> address: ${address}` ;
+}
+let country = "bangladesh"
+let district = "dhaka"
+export default student_info;
+export {country,district};
